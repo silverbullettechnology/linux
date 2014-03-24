c6d34f1f2efd54b0e4aca8a73bad18b2a5e74483
Morgan Hughes <morgan.hughes@silver-bullet-tech.com>
2014-03-24 11:08:49 -0700

    Kconfig changes for CONFIG_PHYS_OFFSET on Zynq
    Removes Zynq dependency on ARCH_MULTIPLATFORM so we can use CONFIG_PHYS_OFFSET for Xilinx
    FreeRTOS AMP per UG978.

---
+++ arch/arm/mach-zynq/include/mach/irqs.h
@@ -0,0 +1,10 @@
+/*
+ * linux/arch/arm/mach-zynq/include/mach/irqs.h
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#define NR_IRQS NR_IRQS_LEGACY
+
