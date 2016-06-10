24feeac7983a7ea8d5e7435d38893e2ea04920cc
Morgan Hughes <morgan.hughes@silver-bullet-tech.com>
2016-06-09 23:10:10 -0700

    Carry-forward tdsdr-devel from 3.12 kernel
    commit 5638579086a152ffc463aca8490767ffcde732d0
    Author: Morgan Hughes <morgan.hughes@silver-bullet-tech.com>
    Date:   Thu Jan 22 17:05:40 2015 -0800
    
        Add support for cs-gpios to Zynq SPI driver

--- drivers/spi/spi-cadence.c
+++ drivers/spi/spi-cadence.c
@@ -181,17 +181,23 @@ static void cdns_spi_init_hw(struct cdns_spi *xspi)
 static void cdns_spi_chipselect(struct spi_device *spi, bool is_high)
 {
 	struct cdns_spi *xspi = spi_master_get_devdata(spi->master);
+	int mode = spi->mode & SPI_CS_HIGH;
 	u32 ctrl_reg;
 
 	ctrl_reg = cdns_spi_read(xspi, CDNS_SPI_CR_OFFSET);
 
 	if (is_high) {
 		/* Deselect the slave */
-		ctrl_reg |= CDNS_SPI_CR_SSCTRL_MASK;
+		if ( spi->cs_gpio >= 0 )
+			gpio_set_value(spi->cs_gpio, !mode);
+		else
+			ctrl_reg |= CDNS_SPI_CR_SSCTRL_MASK;
 	} else {
 		/* Select the slave */
 		ctrl_reg &= ~CDNS_SPI_CR_SSCTRL_MASK;
-		if (!(xspi->is_decoded_cs))
+		if ( spi->cs_gpio >= 0 )
+			gpio_set_value(spi->cs_gpio, !!mode);
+		else if (!(xspi->is_decoded_cs))
 			ctrl_reg |= ((~(CDNS_SPI_SS0 << spi->chip_select)) <<
 				     CDNS_SPI_SS_SHIFT) &
 				     CDNS_SPI_CR_SSCTRL_MASK;
